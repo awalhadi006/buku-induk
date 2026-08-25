97ef562d6c6a3286ecc8bced8a13bd117611533d
Komputer Nyerper <awalhadi006@gmail.com>
2026-08-25 10:48:23 +0700

    chore: exclude folder error dan tests e2e playwright dari vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    exclude: ['**/node_modules/**', '**/error/**', '**/dist/**', 'tests/**'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html', 'lcov'],
